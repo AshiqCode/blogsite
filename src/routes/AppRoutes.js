5fa17b766dd83078e485043c6e7fe826e8d946c3
AshiqCode <muhammadashiq.dev@gmail.com>
2026-02-07 23:49:57 +0500

    added analytics

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,9 +1,8 @@
-import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 
 import Home from "../pages/user/Home";
 import PostDetails from "../pages/user/PostDetails";
-
 import AdminLayout from "../pages/admin/AdminLayout";
 import AdminDashboard from "../pages/admin/AdminDashboard";
 import AdminNewPost from "../pages/admin/AdminNewPost";
@@ -11,17 +10,22 @@ import AdminEditPost from "../pages/admin/AdminEditPost";
 
 export default function AppRoutes() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/post/:slug" element={<PostDetails />} />
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/post/:slug" element={<PostDetails />} />
 
-      <Route path="/blogadmincode4" element={<AdminLayout />}>
-        <Route index element={<AdminDashboard />} />
-        <Route path="new" element={<AdminNewPost />} />
-        <Route path="edit/:id" element={<AdminEditPost />} />
-      </Route>
+        <Route path="/blogadmincode4" element={<AdminLayout />}>
+          <Route index element={<AdminDashboard />} />
+          <Route path="new" element={<AdminNewPost />} />
+          <Route path="edit/:id" element={<AdminEditPost />} />
+        </Route>
 
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+
+      {/* This tracks route changes */}
+      <Analytics />
+    </>
   );
 }
