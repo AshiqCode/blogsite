e884bb3457163a2f5976de97d68647df418ddfa8
AshiqCode <muhammadashiq.dev@gmail.com>
2026-02-08 12:02:17 +0500

    added robots text

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { Helmet } from "react-helmet-async";
 import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import AppRoutes from "./routes/AppRoutes";
@@ -6,6 +6,16 @@ import AppRoutes from "./routes/AppRoutes";
 export default function App() {
   return (
     <BrowserRouter>
+      <Helmet>
+        <title>TrendScope – Insights, Blogs & Trending Topics</title>
+        <meta
+          name="description"
+          content="Read insights, stories, and expert ideas from the TrendScope team. Discover trending blogs and in-depth articles shaping today—read now!"
+        />
+        <link rel="canonical" href="https://trendscope.site/" />
+        <meta name="robots" content="index, follow" />
+      </Helmet>
+
       <Navbar />
       <AppRoutes />
     </BrowserRouter>
